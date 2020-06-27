58a0c6fd8b48273247820db93f60d42a097f776c
L.T.Any <linetoanywhere@gmail.com>
2020-06-27 15:24:39 +0800

    update

---
+++ key/sfh/config.js
@@ -0,0 +1,7 @@
+window.global_config = {
+  isDoLogin: true,
+  //BASE_URL: "http://tomcat.xizue.com:8085",
+  //BASE_URL:"http://xiexinxin.ngrok2.xiaomiqiu.cn"
+ // BASE_URL: "http://aaa.java.imyjava.top"
+  BASE_URL: 'http://superbrain.hainingpige.com'
+};
